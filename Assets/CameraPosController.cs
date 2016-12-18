6d9141a44c4a2fb60f5bc20c039d11f616f4598a
elmir <elmir@brab>
2016-12-18 14:37:11 +0100

    camera yaw and pitch control implemented

---
+++ Assets/CameraPosController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPosController : MonoBehaviour
+{
+    float panTiltSpeed = 64f;
+
+    float yaw = 0.0f;
+    float pitch = 0.0f;
+
+
+    void YawPitch()
+    {
+        var rate = Time.deltaTime * panTiltSpeed;
+
+        var horizontal = Input.GetAxis("Horizontal");
+        if (Mathf.Abs(horizontal) > 0.6)
+        {
+            yaw +=  rate * horizontal;
+        }
+
+        var vertical = Input.GetAxis("Vertical");
+        if (Mathf.Abs(vertical) > 0.001)
+        {
+            pitch -= rate * vertical;
+        }
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+
+	void Update()
+    {
+        YawPitch();
+	}
+}
